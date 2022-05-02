a7777bc623a5c4afcb21baa3c4454cdfe69cf9b1
Bollineni <vbollineni@teksystems.com>
2022-05-02 11:47:40 +0530

    stereotype annotations

---
+++ springcore1/src/main/java/com/springdemos/springcore1/stereotype/annotations/Coach.java
@@ -0,0 +1,30 @@
+package com.springdemos.springcore1.stereotype.annotations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("supercoach")
+@Scope("prototype")
+public class Coach {
+	@Value("11")
+	private int id;
+	@Value("Max")
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Coach [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ springcore1/src/main/java/com/springdemos/springcore1/stereotype/annotations/Test.java
@@ -0,0 +1,17 @@
+package com.springdemos.springcore1.stereotype.annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("com/springdemos/springcore1/stereotype/annotations/config.xml");
+		
+		Coach ch=(Coach)context.getBean("supercoach");
+		
+		System.out.println(ch);
+		System.out.println(ch.hashCode());
+		
+		Coach ch1=(Coach)context.getBean("supercoach");
+		System.out.println(ch1.hashCode());
+	}
+}
